14b3fe40336c862cb41f6eb5300d8ab97daf1df1
YrShavlak <shavlak1939@gmail.com>
2024-01-18 17:14:19 +0100

    Practice3

--- src/main/Java/StringManipulation1.java
+++ src/main/Java/StringManipulation1.java
@@ -4,25 +4,21 @@ public class StringManipulation1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Ввод строки
         System.out.print("Enter string: ");
         String inputString = scanner.nextLine();
 
-        // Вывод первого символа
         if (!inputString.isEmpty()) {
             System.out.println("First Symbol: " + inputString.charAt(0));
         } else {
             System.out.println("String is empty.");
         }
 
-        // Вывод последнего символа
         if (!inputString.isEmpty()) {
             System.out.println("Last Symbol: " + inputString.charAt(inputString.length() - 1));
         } else {
             System.out.println("String is empty.");
         }
 
-        // Вывод среднего символа (если он есть)
         if (inputString.length() % 2 == 1) {
             int middleIndex = inputString.length() / 2;
             System.out.println("Middle Symbol: " + inputString.charAt(middleIndex));
@@ -30,7 +26,6 @@ public class StringManipulation1 {
             System.out.println("No middle symbol");
         }
 
-        // Вывод части строки до первой точки, включая точку
         int dotIndex = inputString.indexOf('.');
         if (dotIndex != -1) {
             String substringBeforeDot = inputString.substring(0, dotIndex + 1);
@@ -39,7 +34,6 @@ public class StringManipulation1 {
             System.out.println("Dot is not found in the string.");
         }
 
-        // Вывод количества пробелов/
         int spaceCount = 0;
         for (int i = 0; i < inputString.length(); i++) {
             if (Character.isWhitespace(inputString.charAt(i))) {
